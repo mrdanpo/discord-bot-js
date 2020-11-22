2880213f7505fc4d3761f58463c606ba22ecd43b
Daniel Proctor <danielproctor@mailbox.org>
2020-11-22 21:29:44 +0000

    fix gifs

--- src/commands/gif.js
+++ src/commands/gif.js
@@ -4,9 +4,9 @@ module.exports = {
 	name: 'gif',
 	description: 'gif',
 	async execute(message, args) {
-        await fetch(`https://api.tenor.com/v1/search?q=${args[0]}&contentFilter=off&key=${process.env.TENOR_API_KEY}`);
-        response = await response.json();
+        var response = await fetch(`https://api.tenor.com/v1/search?q=${args[0]}&contentFilter=off&key=${process.env.TENOR_API_KEY}`);
+        var result = await response.json();
 
-        message.channel.send(response.results[Math.floor((Math.random() * 20) + 0)].url);
+        message.channel.send(result.results[Math.floor((Math.random() * 20) + 0)].url);
 	},
 };
